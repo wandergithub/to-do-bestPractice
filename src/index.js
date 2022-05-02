3edfbba93c6a57093dbb95e3589d9084345cb733
wandergithub <wandergonzalez0@hotmail.com>
2022-05-02 15:44:45 -0400

    add linters

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import './style.css';
+import './assets/images/refresh.png';
+import './assets/images/arrow.png';
+import './assets/images/dot.png';
+import './assets/images/trash-can.png';
+import userActionHandler from './modules/userActions.js';
+import { initStorage } from './modules/localStorage.js';
+import taskStatus from './modules/taskStatus.js';
+import butonHandler from './modules/clearAll.js';
+
+butonHandler;
+userActionHandler();
+window.addEventListener('load', initStorage);
+window.addEventListener('load', taskStatus);
